ea59deb579f19baf12229ebbdc5183ed34412ce4
orongdeveloper <orongdeveloper@gmail.com>
2023-05-19 06:20:25 -0700

    database

--- registration.php
+++ registration.php
@@ -1,43 +1,30 @@
-<?php 
+<?php
 
 session_start();
 
+$con = mysqli_connect('54.153.243.37', 'root', 'root');
 
-$con=mysqli_connect('localhost','root');
+if ($con) {
+    echo "connection successful";
+} else {
+    echo "no connection";
+}
 
-if ($con) 
-  {
-		echo "connection successful";
-  }
-else
-	{
-		echo "no connection";
-	}
+mysqli_select_db($con, 'uniquedeveloper');
+$name = $_POST['name'];
+$pass = $_POST['password'];
+$email = $_POST['email'];
 
- mysqli_select_db($con,'uniquedeveloper');
- $name=$_POST['name'];
- $pass=$_POST['password'];
- $email=$_POST['email'];
+$q = "select * from login where username='$name' && password='$pass'";
 
- $q="select * from login where username='$name' && password='$pass'";
-
- $result=mysqli_query($con,$q);
- $num=mysqli_num_rows($result);
- if ($num==1)
-  {
- 	"duplicate data";
- 	header('location:signup.html');
- }
- else
- {
- 	$qy="insert into login(username,password,email) values('$name','$pass','$email')";
- 	mysqli_query($con,$qy);
- 	echo "inserted";
- 	header('location:login.php');
- }
-
-
-
-
-
- ?>
+$result = mysqli_query($con, $q);
+$num = mysqli_num_rows($result);
+if ($num == 1) {
+    "duplicate data";
+    header('location:signup.html');
+} else {
+    $qy = "insert into login(username,password,email) values('$name','$pass','$email')";
+    mysqli_query($con, $qy);
+    echo "inserted";
+    header('location:login.php');
+}
--- validation.php
+++ validation.php
@@ -1,53 +1,37 @@
-<?php 
+<?php
 
 session_start();
 
+$con = mysqli_connect('54.153.243.37', 'root', 'root');
 
-$con=mysqli_connect('localhost','root');
+if ($con) {
+    echo "connection successful";
+} else {
+    echo "no connection";
+}
 
-if ($con) 
-  {
-		echo "connection successful";
-  }
-else
-	{
-		echo "no connection";
-	}
+mysqli_select_db($con, 'uniquedeveloper');
+$name = $_POST['name'];
+$pass = $_POST['password'];
+$email = $_POST['email'];
 
- mysqli_select_db($con,'uniquedeveloper');
- $name=$_POST['name'];
- $pass=$_POST['password'];
- $email=$_POST['email'];
+$q = "select * from login where username='$name' && password='$pass'";
 
- $q="select * from login where username='$name' && password='$pass'";
+$result = mysqli_query($con, $q);
+$res = mysqli_fetch_assoc($result);
+$num = mysqli_num_rows($result);
+if ($num == 1) {
 
- $result=mysqli_query($con,$q);
- $res=mysqli_fetch_assoc($result);
- $num=mysqli_num_rows($result);
- if ($num==1)
-  {
+    if ($res['username'] == 'admin') {
+        header("location:admin/admin_main.php");
 
-  	if ($res['username']=='admin') 
-  	{
-  		header("location:admin/admin_main.php");
-  		
-  	}
-  	else
-  	{
+    } else {
 
- 	$_SESSION['username']=$name;
- 	header('location:index.php');
- 	}
- }
- else
- {
- 	$_SESSION['error']="error";
- 	header('location:login.php');
+        $_SESSION['username'] = $name;
+        header('location:index.php');
+    }
+} else {
+    $_SESSION['error'] = "error";
+    header('location:login.php');
 
- }
-
-
-
-
-
- ?>
+}
